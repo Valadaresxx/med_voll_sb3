3ce82a71e599c9fe0a9cc1d67e55dc485478f3ec
Gabriel Valadares <silva44gabriel@gmail.com>
2025-08-01 18:11:22 -0300

    feat: adiciona return 201 created

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -4,9 +4,12 @@ import jakarta.validation.Valid;
 import med.voll.api.domain.paciente.DadosCadastroPaciente;
 import med.voll.api.domain.paciente.Paciente;
 import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.paciente.DadosDetalhamentoPaciente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -16,9 +19,12 @@ public class PacienteController {
     private PacienteRepository repository;
 
     @PostMapping
-    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+    @Transactional
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroPaciente dados, UriComponentsBuilder uriBuilder) {
         var paciente = new Paciente(dados);
         repository.save(paciente);
-        return ResponseEntity.ok().build();
+
+        var uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
     }
 }
---
+++ api/src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,14 @@
+package med.voll.api.paciente;
+
+import med.voll.api.domain.endereco.Endereco;
+import med.voll.api.domain.paciente.Paciente;
+
+public record DadosDetalhamentoPaciente(String nome,
+                                        String email,
+                                        String cpf,
+                                        Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getEndereco());
+    }
+}
